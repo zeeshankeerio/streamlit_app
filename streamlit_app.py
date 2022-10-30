b483a16449fa3a85ef5b03146c62bbe43970b6c8
Zeeshan Keerio <51040406+zeeshankeerio@users.noreply.github.com>
2022-10-30 12:42:37 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,9 @@
 import streamlit
-streamlit.title('ZEESHAN KEERIO')
-streamlit.header('About')
-streamlit.text(''' I love to dig into data analytics problems, solve them with modern technologies, 
-create new ideas, and translate business requirements into technical solutions. To accomplish this, 
-I focus on delivering key outcomes, building possible analytical solutions, and quickly adapting to 
-new learnings. I have enjoyed many machine learning, live data integration, prescriptive analytics, 
-and business analytics problems throughout my career. My Freelancing and professional experiences have 
-taught me the following about myself: Multi tasking, working under pressure, performing data modeling, 
-diagnostics on existing reports, developing data solutions, scaling, visualizing, transforming big-time 
-series data, building ml models, and optimizing business problems that matter. 
+stramlit.title('New Healty Dinner')
 
-I educate, refine and drive myself to be a better person, staying calm when faced with adversity, I am constantly 
-learning because I never settle, focusing on making high-quality decisions & experienced in learning and excelling 
-at new technologies as needed.
+streamlit.header('Breakfast Menu')
+streamlit.text( '🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Advocado Toast')
 
-I enjoy meeting new people, and I do not like pushy people. If you are nice, I will go out of my way to help you. 
-I like to treat people with respect, and I love to learn about new technologies and hear new perspectives. Reach out 
-if you want to talk to me about emerging tech, solving data analytics, machine learning problems, or consultation.''')
